0dc2cdc61c6872bad106879935016d89d1e77f71
ihpangzi <xiefuzhong@vip.qq.com>
2023-02-09 13:35:46 +0800

    Category基础代码

--- src/main/java/com/acme/acmemall/service/impl/CategoryService.java
+++ src/main/java/com/acme/acmemall/service/impl/CategoryService.java
@@ -2,10 +2,12 @@ package com.acme.acmemall.service.impl;
 
 import com.acme.acmemall.model.CategoryVo;
 import com.acme.acmemall.service.ICategoryService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class CategoryService implements ICategoryService {
     /**
      * @param map
