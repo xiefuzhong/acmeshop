9a3ead99e47a10ff9edbabfdc5c47d7e4367a9e9
ihpangzi <xiefuzhong@vip.qq.com>
2023-02-10 18:14:18 +0800

    微信授权登录相关代码-启动类增加bean初始化

--- src/main/java/com/acme/acmemall/StartupApplication.java
+++ src/main/java/com/acme/acmemall/StartupApplication.java
@@ -3,6 +3,9 @@ package com.acme.acmemall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.acme.acmemall.dao"})
@@ -10,5 +13,11 @@ public class StartupApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(StartupApplication.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder)
+  {
+    return builder.build();
   }
 }
