cc68317757b183cb5e53629d686fc0ecf42e83c1
ihpangzi <xiefuzhong@vip.qq.com>
2023-02-13 15:53:53 +0800

    api层拦截器-用户信息-日志

--- src/main/java/com/acme/acmemall/resolver/ConfigAdaper.java
+++ src/main/java/com/acme/acmemall/resolver/ConfigAdaper.java
@@ -1,8 +1,10 @@
 package com.acme.acmemall.resolver;
 
+import com.acme.acmemall.interceptor.AuthorizationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -17,9 +19,21 @@ public class ConfigAdaper extends WebMvcConfigurerAdapter {
     @Autowired
     LoginUserHandlerMethodArgumentResolver loginUserHandlerMethodArgumentResolver;
 
+    @Autowired
+    AuthorizationInterceptor authorizationInterceptor;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         super.addArgumentResolvers(argumentResolvers);
         argumentResolvers.add(loginUserHandlerMethodArgumentResolver);
+    }
+
+    /**
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        registry.addInterceptor(authorizationInterceptor).addPathPatterns("/api/**");
     }
 }
