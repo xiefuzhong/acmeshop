6827fb06247a4e89c879db1cdeb0d2bb7d701621
ihpangzi <xiefuzhong@vip.qq.com>
2023-02-24 09:46:29 +0800

    订单提交-添加日志用于定位

--- src/main/java/com/acme/acmemall/controller/OrderController.java
+++ src/main/java/com/acme/acmemall/controller/OrderController.java
@@ -1,19 +1,17 @@
 package com.acme.acmemall.controller;
 
 import com.acme.acmemall.annotation.LoginUser;
+import com.acme.acmemall.common.ResultMap;
 import com.acme.acmemall.controller.reqeust.OrderSubmitRequest;
 import com.acme.acmemall.model.LoginUserVo;
 import com.acme.acmemall.service.IOrderService;
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  * @description:订单相关
@@ -31,13 +29,14 @@ public class OrderController extends ApiBase {
     @ApiOperation(value = "订单提交")
     @PostMapping("submit")
     public Object submit(@LoginUser LoginUserVo loginUser) {
-        Map resultObj = null;
+        ResultMap resultObj = null;
         try {
             OrderSubmitRequest request = JSONObject.toJavaObject(getJsonRequest(), OrderSubmitRequest.class);
             request.check();
             resultObj = orderService.submit(request, loginUser);
             if (null != resultObj) {
-                return toResponsObject(MapUtils.getInteger(resultObj, "errno"), MapUtils.getString(resultObj, "errmsg"), resultObj.get("data"));
+                logger.info("Order.submit()=="+JSONObject.toJSONString(resultObj));
+                return resultObj;
             }
         } catch (Exception e) {
             e.printStackTrace();
