fe8c4de979a184105198ca95413e36372437dbe8
xiefuzhong <xiefuzhong@vip.qq.com>
2024-01-09 19:19:37 +0800

    素材管理功能

--- src/main/java/com/acme/acmemall/model/ProductMaterialsVo.java
+++ src/main/java/com/acme/acmemall/model/ProductMaterialsVo.java
@@ -18,7 +18,7 @@ public class ProductMaterialsVo implements Serializable {
     private String filePath; // 文件相对路径
     private String fileType; // 文件类型 image/
     private String fileUrl;  // 文件地址用于访问
-    private long fileSize;      // 文件地址 byte
+    private long size;      // 文件地址 byte
     private String tempFilePath; // 上传地址(本地/微信)
     private long userId; // uploader上传人
     private Integer uploadType; // 文件来源：相册/文件系统
