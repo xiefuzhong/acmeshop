35c96fe02a222628646d86c2f2a0c21a1fc9291c
xiefuzhong <xiefuzhong@vip.qq.com>
2024-01-17 10:17:38 +0800

    基础功能-拦截器-增加异常明细输出

--- src/main/java/com/acme/acmemall/acpect/ControllerAspect.java
+++ src/main/java/com/acme/acmemall/acpect/ControllerAspect.java
@@ -1,13 +1,14 @@
 package com.acme.acmemall.acpect;
 
+import com.acme.acmemall.utils.GsonUtil;
+import com.google.common.base.Throwables;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 
 /**
@@ -27,11 +28,21 @@ public class ControllerAspect {
     @Around("point()")
     public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
         long begin = System.nanoTime();
-        Object result = pjp.proceed();
+        Object returnValue = null;
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        // 请求的方法名
+        String methodName = signature.getName();
+        try {
+            returnValue = pjp.proceed();
+        } catch (Throwable throwable) {
+            String errorMessage = String.format("请求异常：请求方法：{s%}, 异常信息：{s%}", methodName, Throwables.getStackTraceAsString(throwable));
+            logger.error(errorMessage);
+            throw throwable;
+        }
         long end = System.nanoTime();
-        long cost = end-begin;
-        logger.info("Controller method："+pjp.getSignature().toString()+"，prams："+(pjp.getArgs().length>0? Arrays.toString(pjp.getArgs()):"void")+"，cost time："+cost+" ns");
-        return result;
+        long cost = end - begin;
+        logger.info("Controller method：" + pjp.getSignature().getName() + "，prams：" + (pjp.getArgs().length > 0 ? GsonUtil.getGson().toJson(pjp.getArgs()) : "void") + "，cost time：" + cost + " ns");
+        return returnValue;
     }
 
 }
