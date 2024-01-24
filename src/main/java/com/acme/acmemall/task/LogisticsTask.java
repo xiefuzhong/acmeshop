577006064bb5d07d843407cefc0bd006125224d8
xiefuzhong <xiefuzhong@vip.qq.com>
2024-01-24 21:40:27 +0800

    订单管理-发货-物流对接(快100)-同步快递信息

--- src/main/java/com/acme/acmemall/task/LogisticsTask.java
+++ src/main/java/com/acme/acmemall/task/LogisticsTask.java
@@ -10,6 +10,7 @@ import com.acme.acmemall.utils.GsonUtil;
 import com.acme.acmemall.utils.MapUtils;
 import com.acme.acmemall.utils.StringUtils;
 import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.log4j.Logger;
@@ -21,7 +22,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 @EnableScheduling
@@ -56,17 +56,26 @@ public class LogisticsTask {
             logger.info(String.format("查询数据，执行时间：%s,查无数据处理", DateUtils.currentDate(DateUtils.DATE_TIME_PATTERN)));
             return;
         }
-        List firstResultList = resultList.stream().filter(item -> StringUtils.isNullOrEmpty(item.get("waybill_id").toString())).collect(Collectors.toList());
-        if (CollectionUtils.isEmpty(firstResultList)) {
+        List<Map> list = Lists.newArrayList();
+        // 过滤出来 waybill_id 为空的记录，取同步数据
+        for (int i = 0; i < resultList.size(); i++) {
+            Map itemMap = resultList.get(i);
+            String waybillId = MapUtils.getString("waybill_id", itemMap);
+            if (StringUtils.isNullOrEmpty(waybillId)) {
+                list.add(itemMap);
+            }
+        }
+
+        if (CollectionUtils.isEmpty(list)) {
             logger.info(String.format("过滤数据，执行时间：%s,查无数据处理", DateUtils.currentDate(DateUtils.DATE_TIME_PATTERN)));
             return;
         }
         resultList.clear();
         // 收集ORDER_ID
-        List paddingList = firstResultList;
-        if (firstResultList.size() > 50) {
-            paddingList = firstResultList.subList(0, 50);
-            firstResultList.clear();
+        List paddingList = list;
+        if (list.size() > 50) {
+            paddingList = list.subList(0, 50);
+            list.clear();
         }
         try {
 //            List<OrderDeliveryTrackVo> deliveryTrackVoList = Lists.newArrayList();
