fa2eb5bea10676042daab6b1f80f3fc5cd8826d7
xiefuzhong <xiefuzhong@vip.qq.com>
2024-01-30 11:49:24 +0800

    商品管理-商品详情补充

--- src/main/java/com/acme/acmemall/controller/GoodsController.java
+++ src/main/java/com/acme/acmemall/controller/GoodsController.java
@@ -477,7 +477,6 @@ public class GoodsController extends ApiBase {
             return ResultMap.error();
         }
         GoodsManageRequest manageRequest = JSONObject.toJavaObject(jsonRequest, GoodsManageRequest.class);
-
         return goodsService.updateGoods(manageRequest, loginUserVo);
     }
 
