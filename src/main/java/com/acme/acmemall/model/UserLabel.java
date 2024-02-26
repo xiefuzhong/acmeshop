52da57118c0571ee9edd6c57ae014d02ec879d51
xiefuzhong <xiefuzhong@vip.qq.com>
2024-02-26 21:17:28 +0800

    用户

--- src/main/java/com/acme/acmemall/model/UserLabel.java
+++ src/main/java/com/acme/acmemall/model/UserLabel.java
@@ -1,9 +1,6 @@
 package com.acme.acmemall.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
 import java.io.Serializable;
 
@@ -12,10 +9,7 @@ import java.io.Serializable;
  * @author: ihpangzi
  * @time: 2024/2/20 10:04
  */
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class UserLabel implements Serializable {
     //    [{"checked":true,"id":2,"title":"设计总监"}]
     private long id;
