d6ffbddc364d54d16eee05795b1985fa697d1d6c
xiefuzhong <xiefuzhong@vip.qq.com>
2024-03-01 17:43:38 +0800

    退款订单

--- src/main/java/com/acme/acmemall/model/OrderHandleOption.java
+++ src/main/java/com/acme/acmemall/model/OrderHandleOption.java
@@ -1,13 +1,9 @@
 package com.acme.acmemall.model;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.Map;
 
 /**
  * @description:
@@ -81,49 +77,49 @@ public class OrderHandleOption {
      *
      * @param statusCode
      */
-    public Map<String, Boolean> canOption(int statusCode) {
-        OrderStatus status = OrderStatus.parse(statusCode);
-        switch (status) {
-            case TO_BE_PAID: {
-                this.pay = Boolean.TRUE;
-                this.cancel = Boolean.TRUE;
-                this.updateAddress = Boolean.TRUE;
-                this.mark = Boolean.TRUE;
-                break;
-            }
-            case CANCELED: {
-                this.delete = Boolean.TRUE;
-                this.buy = Boolean.TRUE;
-                break;
-            }
-            case TO_BE_SHIPPED: {
-                this.cancel = Boolean.TRUE;
-                this.updateAddress = Boolean.TRUE;
-                this.mark = Boolean.TRUE;
-                this.print = Boolean.TRUE;
-                break;
-            }
-            case TO_BE_RECEIVED: {
-                this.confirm = Boolean.TRUE;
-                break;
-            }
-            case COMPLETED: {
-                this.comment = Boolean.TRUE;
-                this.buy = Boolean.TRUE;
-                this.returned = Boolean.TRUE;
-                break;
-            }
-            case PAID: {
-                this.mark = Boolean.TRUE;
-                this.print = Boolean.TRUE;
-                this.updateAddress = Boolean.TRUE;
-                this.toShipping = Boolean.TRUE;
-                this.cancel = Boolean.TRUE;
-                break;
-            }
-        }
-//        System.out.println("option-->" + JSON.toJSONString(this));
-        return JSON.parseObject(JSON.toJSONString(this), new TypeReference<Map<String, Boolean>>() {
-        });
-    }
+//    public Map<String, Boolean> canOption(int statusCode) {
+//        OrderStatus status = OrderStatus.parse(statusCode);
+//        switch (status) {
+//            case TO_BE_PAID: {
+//                this.pay = Boolean.TRUE;
+//                this.cancel = Boolean.TRUE;
+//                this.updateAddress = Boolean.TRUE;
+//                this.mark = Boolean.TRUE;
+//                break;
+//            }
+//            case CANCELED: {
+//                this.delete = Boolean.TRUE;
+//                this.buy = Boolean.TRUE;
+//                break;
+//            }
+//            case TO_BE_SHIPPED: {
+//                this.cancel = Boolean.TRUE;
+//                this.updateAddress = Boolean.TRUE;
+//                this.mark = Boolean.TRUE;
+//                this.print = Boolean.TRUE;
+//                break;
+//            }
+//            case TO_BE_RECEIVED: {
+//                this.confirm = Boolean.TRUE;
+//                break;
+//            }
+//            case COMPLETED: {
+//                this.comment = Boolean.TRUE;
+//                this.buy = Boolean.TRUE;
+//                this.returned = Boolean.TRUE;
+//                break;
+//            }
+//            case PAID: {
+//                this.mark = Boolean.TRUE;
+//                this.print = Boolean.TRUE;
+//                this.updateAddress = Boolean.TRUE;
+//                this.toShipping = Boolean.TRUE;
+//                this.cancel = Boolean.TRUE;
+//                break;
+//            }
+//        }
+////        System.out.println("option-->" + JSON.toJSONString(this));
+//        return JSON.parseObject(JSON.toJSONString(this), new TypeReference<Map<String, Boolean>>() {
+//        });
+//    }
 }
