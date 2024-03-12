988ac22cd82ae7b3b349d289242cb2d5310b09d6
xiefuzhong <xiefuzhong@vip.qq.com>
2024-03-12 11:17:06 +0800

    售后服务13

--- src/main/java/com/acme/acmemall/model/OrderVo.java
+++ src/main/java/com/acme/acmemall/model/OrderVo.java
@@ -727,7 +727,7 @@ public class OrderVo implements Serializable {
             // 仅退款(审核通过&&已付款)
             return this.refund_status == RefundStatusEnum.REFUND_PASS.getCode()
                     && this.pay_status == PayStatusEnum.PAY_YES.getCode();
-        } else if (this.refundVo.getRefund_type() == 2) {
+        } else if (refundVo.getRefund_type() == 2) {
             // 货物退回 && 商家签收
             if (this.shipping_status == ShipStatusEnum.SHIP_RETURN.getCode()
                     && this.refund_status == RefundStatusEnum.REFUND_RETURNED.getCode()
