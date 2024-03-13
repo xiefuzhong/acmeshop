f20a76d2948e07f994d64ca9797d19ec779c5cf6
xiefuzhong <xiefuzhong@vip.qq.com>
2024-03-13 11:33:08 +0800

    售后服务-撤销售后7

--- src/main/java/com/acme/acmemall/model/OrderRefundVo.java
+++ src/main/java/com/acme/acmemall/model/OrderRefundVo.java
@@ -51,6 +51,7 @@ public class OrderRefundVo implements Serializable {
     private String refund_phone;
     private String refund_express;
     private String refund_express_name;
+    private String refund_express_code;
     private String refuse_reason;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
