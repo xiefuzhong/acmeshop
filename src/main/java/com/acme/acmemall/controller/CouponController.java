d7298c75e04350832960d47dad4f3c218054fd12
xiefuzhong <xiefuzhong@vip.qq.com>
2024-03-26 18:36:40 +0800

    feat(营销中心): 优惠券 1.用户优惠券列表增加状态查询条件

--- src/main/java/com/acme/acmemall/controller/CouponController.java
+++ src/main/java/com/acme/acmemall/controller/CouponController.java
@@ -69,9 +69,9 @@ public class CouponController extends ApiBase {
     @ApiOperation(value = "获取用户可用优惠券列表")
     @GetMapping("/valid-coupon-list")
     public Object getValidCouponList(@LoginUser LoginUserVo loginUser,
-                                     @RequestParam("merchantId", defaultValue = "0") Long merchantId,
-                                     @RequestParam("goodsTotalPrice", defaultValue = "1") BigDecimal goodsTotalPrice,
-                                     @RequestParam("coupon_status", defaultValue = "0") Integer coupon_status) {
+                                     Long merchantId,
+                                     BigDecimal goodsTotalPrice,
+                                     Integer coupon_status) {
         Map param = Maps.newHashMap();
         param.put("user_id", loginUser.getUserId());
         param.put("merchantId", merchantId);
