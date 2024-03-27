72b70a761830512a3934cd9000fd79ddbcf7bc2f
xiefuzhong <xiefuzhong@vip.qq.com>
2024-03-27 16:46:01 +0800

    feat(营销中心): 优惠券 1.用户优惠券列表增加状态查询条件-2

--- src/main/java/com/acme/acmemall/controller/UserController.java
+++ src/main/java/com/acme/acmemall/controller/UserController.java
@@ -139,10 +139,6 @@ public class UserController extends ApiBase {
         if (loginUser == null) {
             return ResultMap.error(400, "非有效用户操作");
         }
-        LoginUserVo userVo = userService.queryByUserId(loginUser.getUserId());
-        if (userVo == null || userVo.getUserId() == 0) {
-            return ResultMap.error(1001, "请先登录管理系统再操作!");
-        }
         LoginUserVo userVo2 = userService.queryObject(userId);
         if (userVo2 == null) {
             return ResultMap.badArgumentValue("userId查无数据");
