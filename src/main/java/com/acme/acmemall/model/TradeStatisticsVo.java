b147384187dc15c9da60837398eb72a2fa3643e1
xiefuzhong <xiefuzhong@vip.qq.com>
2024-04-29 14:13:43 +0800

    feat(财务): 交易统计

---
+++ src/main/java/com/acme/acmemall/model/TradeStatisticsVo.java
@@ -0,0 +1,37 @@
+package com.acme.acmemall.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * @description:交易统计
+ * @author: ihpangzi
+ * @time: 2024/04/29 12:00
+ */
+@Data
+public class TradeStatisticsVo implements Serializable {
+    private String type; //   交易类型
+    private String title; //   交易标题
+    private Integer num; //   交易数量
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "##0.00")
+    private BigDecimal amount; //交易金额
+
+    private long amountRate; //交易金额
+
+    public BigDecimal getAmount() {
+        return amount == null ? BigDecimal.ZERO : amount.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public long getAmountRate() {
+        if (amount == null) {
+            return 0L;
+        }
+        return amount.multiply(new BigDecimal(100)).longValue();
+    }
+
+
+}
