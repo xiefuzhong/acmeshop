a2d5d5b298c10c2b4be4b6cee3977bec56142e80
xiefuzhong <xiefuzhong@vip.qq.com>
2024-05-03 10:30:56 +0800

    feat(微信物流助手): API对接

--- src/main/java/com/acme/acmemall/model/Service.java
+++ src/main/java/com/acme/acmemall/model/Service.java
@@ -12,8 +12,8 @@ import lombok.*;
 @Builder
 @Getter
 public class Service {
-    private long expect_time; // 如果是下顺丰散单，则必传此字段，否则不会有收件员上门揽件。
-    private int take_mode; // 分单策略;
+    //    private long expect_time; // 如果是下顺丰散单，则必传此字段，否则不会有收件员上门揽件。
+//    private int take_mode; // 分单策略;
     private Integer service_type;
     private String service_name;
 }
