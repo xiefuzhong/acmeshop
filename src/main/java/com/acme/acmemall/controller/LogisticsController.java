ed2aa4972a321673593f9461339b779a2000457b
xiefuzhong <xiefuzhong@vip.qq.com>
2024-05-10 21:15:12 +0800

    feat(订单): 物流轨迹查询3

--- src/main/java/com/acme/acmemall/controller/LogisticsController.java
+++ src/main/java/com/acme/acmemall/controller/LogisticsController.java
@@ -1,8 +1,16 @@
 package com.acme.acmemall.controller;
 
+import com.acme.acmemall.annotation.LoginUser;
+import com.acme.acmemall.cainiao.service.LogisticsCloudService;
+import com.acme.acmemall.cainiao.vo.TraceVo;
+import com.acme.acmemall.common.ResultMap;
+import com.acme.acmemall.model.LoginUserVo;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @description:物流信息
@@ -13,9 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/logistics")
 public class LogisticsController extends ApiBase {
 
-    @PostMapping("/update")
-    public String updateLogistics() {
+    @Resource
+    LogisticsCloudService logisticsCloudService;
 
-        return "success";
+    @PostMapping("/traces")
+    public Object queryExpressRoutes(@LoginUser LoginUserVo loginUser) {
+        if (loginUser == null) {
+            return ResultMap.error("请先登录");
+        }
+        JSONObject jsonObject = getJsonRequest();
+        if (jsonObject == null) {
+            return ResultMap.error("参数错误");
+        }
+        String expressNo = jsonObject.getString("expressNo");
+        String result = logisticsCloudService.queryExpressRoutes(expressNo);
+        JSONObject object = JSONObject.parseObject(result);
+        TraceVo traceVo = JSONObject.toJavaObject(object, TraceVo.class);
+        return toResponsSuccess(traceVo);
     }
 }
